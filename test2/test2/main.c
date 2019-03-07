46db42a6b8a8a5ec491aee59bc66273769e5c669
Han han <hanhan@zh-macbook-pro.local>
2019-03-07 20:14:15 +0800

    2

---
+++ test2/test2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test2
+//
+//  Created by hanhan on 2019/3/7.
+//  Copyright © 2019 hanhan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for(int i=0;i<3;i++)
+    {
+    printf("Hello, World!\n");
+    }
+    return 0;
+}
